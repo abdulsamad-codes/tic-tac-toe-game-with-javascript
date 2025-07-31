ee07892fadab649eeb714bb95094af9765f0e84c
abdul-samadcodes <abdul.samad538541@gmail.com>
2025-08-01 01:22:41 +0500

    third commit

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const winPatterns = [
 
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
-    console.log("box was clicked ");
+    // console.log("box was clicked ");
     if (turnX) {
       box.innerText = "X";
       turnX = false;
@@ -26,7 +26,33 @@ boxes.forEach((box) => {
       turnX = true;
     }
     box.disabled = true;
+    checkWinner();
   });
 });
 
-const checkWinner = () => {};
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let position1Value = boxes[pattern[0]].innerText;
+    let position2Value = boxes[pattern[1]].innerText;
+    let position3Value = boxes[pattern[2]].innerText;
+
+    // console.log(pattern[0], pattern[1], pattern[2]);
+    // console.log(
+    //   boxes[pattern[0]].innerText,
+    //   boxes[pattern[1]].innerText,
+    //   boxes[pattern[2]].innerText
+    // );
+
+    if ((position1Value != "", position2Value != "", position3Value != "")) {
+      if (
+        position1Value === position2Value &&
+        position2Value === position3Value
+      ) {
+        console.log(`Winner is ${position1Value}`);
+      }
+    }
+  }
+};
+// if ((boxes[pattern[0]] === boxes[pattern[1]]) === boxes[pattern[2]]) {
+//       console.log(`The winner is ${boxes[pattern[0]]} .`);
+//     }
